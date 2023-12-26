ba47b057abc5cb807cb5c21b75c4d63f21593c40
Banan Truex <banan@bu.edu>
2023-12-25 19:04:17 -0500

    difficulty increases over time

--- game.py
+++ game.py
@@ -10,12 +10,14 @@ pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 PLAYER_WIDTH, PLAYER_HEIGHT = 50, 50
 OBJECT_WIDTH, OBJECT_HEIGHT = 50, 50
-PLAYER_SPEED = 5    
-OBJECT_MIN_SPEED, OBJECT_MAX_SPEED = 3, 10
+PLAYER_SPEED = 5
+OBJECT_MIN_SPEED, OBJECT_MAX_SPEED = 3, 5  # Initial speed range
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255,0,0)
+RED = (255, 0, 0)
 FONT = pygame.font.SysFont(None, 36)
+
+score_multiplier = 1
 
 class Player:
     def __init__(self, x, y):
@@ -26,14 +28,12 @@ class Player:
         self.speed = PLAYER_SPEED
         self.direction = 1  # 1 for right, -1 for left
 
-    def move(self, x_change, y_change):
-        self.x += x_change
-        self.y += y_change
-        self.x = max(0, min(SCREEN_WIDTH - self.width, self.x))
-        self.y = max(0, min(SCREEN_HEIGHT - self.height, self.y))
-
     def change_direction(self):
         self.direction *= -1
+
+    def move(self):
+        self.x += self.direction * self.speed
+        self.x = max(0, min(SCREEN_WIDTH - self.width, self.x))
 
     def draw(self, screen):
         pygame.draw.rect(screen, BLACK, [self.x, self.y, self.width, self.height])
@@ -45,6 +45,7 @@ class Object:
     def __init__(self):
         self.x = random.randint(0, SCREEN_WIDTH - OBJECT_WIDTH)
         self.y = -OBJECT_HEIGHT
+        global OBJECT_MIN_SPEED, OBJECT_MAX_SPEED
         self.speed = random.randint(OBJECT_MIN_SPEED, OBJECT_MAX_SPEED)
 
     def update(self):
@@ -57,6 +58,7 @@ class Object:
         return pygame.Rect(self.x, self.y, OBJECT_WIDTH, OBJECT_HEIGHT)
 
 def main():
+    global OBJECT_MIN_SPEED, OBJECT_MAX_SPEED, score_multiplier
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Game")
@@ -67,49 +69,41 @@ def main():
     # Object setup
     objects = []
     start_time = time.time()
-    
+
     # Game loop
     running = True
     clock = pygame.time.Clock()
-    x_change = 0
-    y_change = 0
 
+    score = 0
 
     while running:
         screen.fill(WHITE)
 
+        # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:  # Left
-                    x_change = -PLAYER_SPEED
-                elif event.key == pygame.K_d:  # Right
-                    x_change = PLAYER_SPEED
-                elif event.key == pygame.K_w:  # Up
-                    y_change = -PLAYER_SPEED
-                elif event.key == pygame.K_s:  # Down
-                    y_change = PLAYER_SPEED
-            elif event.type == pygame.KEYUP:
-                if event.key in [pygame.K_a, pygame.K_d]:
-                    x_change = 0
-                if event.key in [pygame.K_w, pygame.K_s]:
-                    y_change = 0
+                if event.key == pygame.K_SPACE:
+                    player.change_direction()
 
         # Update player position
-        player.move(x_change, y_change)
-
+        player.move()
 
         # Update objects
         for obj in objects:
             obj.update()
 
         # Add new object
-        # As time increases, make the game harder by increasing the spawn rate of objects
         elapsed_time = time.time() - start_time
-        spawn_threshold = 20 - int(elapsed_time / 10)  # Decrease spawn threshold over time
+        # Decrease spawn threshold over time to increase spawn rate
+        spawn_threshold = max(10, 30 - int(elapsed_time / 20))
         if random.randint(1, spawn_threshold) == 1:
             objects.append(Object())
+
+        # Gradual increase in object speed
+        OBJECT_MIN_SPEED = min(7, 3 + int(elapsed_time / 60))
+        OBJECT_MAX_SPEED = min(10, 5 + int(elapsed_time / 45))
 
         # Draw player
         player.draw(screen)
@@ -122,13 +116,13 @@ def main():
         player_rect = player.get_rect()
         for obj in objects:
             if player_rect.colliderect(obj.get_rect()):
-                running =  False  # End the game on collision
+                running = False  # End the game on collision
 
-        # Calculate score (time survived)
-        score = time.time() - start_time
+        # Scoring
+        score += 1 * score_multiplier  # Update score
 
         # Display score
-        score_text = FONT.render(f'Time Survived: {int(score)} seconds', True, BLACK)
+        score_text = FONT.render(f'Score: {int(score)}', True, BLACK)
         screen.blit(score_text, (10, 10))
 
         # Update the display
