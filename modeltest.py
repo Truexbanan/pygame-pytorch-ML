e0dfc8d9ab190f0c35c896954c54705b804b68d6
Banan Truex <banan@bu.edu>
2023-12-27 11:47:24 -0500

    Update modeltest.py

--- modeltest.py
+++ modeltest.py
@@ -9,3 +9,15 @@ class Linear_QNet(nn.Module):
         super(Linear_QNet, self).__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
