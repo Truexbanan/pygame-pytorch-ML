15a050b86f66f705bc68d707baf9f069e731621b
Banan Truex <banan@bu.edu>
2023-12-27 11:44:50 -0500

    started model

---
+++ modeltest.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class Linear_QNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Linear_QNet, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
